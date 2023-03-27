e397e4a797ffdac76054ea8383b3c8987cdef3c0
Jerome Meuleman <jmeulema@fu-c2-p5.s19.be>
2023-03-27 13:38:38 +0200

    :video_game:

--- Srcs/init.c
+++ Srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: jmeulema <jmeulema@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 17:42:28 by jmeulema          #+#    #+#             */
-/*   Updated: 2023/02/23 16:21:50 by jmeulema         ###   ########.fr       */
+/*   Updated: 2023/03/27 13:37:40 by jmeulema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static void	ft_init_next(t_data *data)
 	int		img_width;
 	int		img_height;
 
-	relative_path = "./img/luffy_right.xpm";
+	relative_path = "./img/Luffy_right.xpm";
 	data->img->player_right = mlx_xpm_file_to_image(data->mlx, relative_path,
 			&img_width, &img_height);
 	relative_path = "./img/background.xpm";
@@ -44,13 +44,13 @@ void	ft_init(t_data *data, t_map *map)
 		exit(EXIT_FAILURE);
 	}
 	data->img = img;
-	relative_path = "./img/luffy_up.xpm";
+	relative_path = "./img/Luffy_up.xpm";
 	data->img->player_up = mlx_xpm_file_to_image(data->mlx, relative_path,
 			&img_width, &img_height);
-	relative_path = "./img/luffy_down.xpm";
+	relative_path = "./img/Luffy_down.xpm";
 	data->img->player_down = mlx_xpm_file_to_image(data->mlx, relative_path,
 			&img_width, &img_height);
-	relative_path = "./img/luffy_left.xpm";
+	relative_path = "./img/Luffy_left.xpm";
 	data->img->player_left = mlx_xpm_file_to_image(data->mlx, relative_path,
 			&img_width, &img_height);
 	ft_init_next(data);
